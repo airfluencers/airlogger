50405e8918e2872aa30a54c4b230c826416bd1be
Thiago Arthur <thiago.goslar@inovamind.com.br>
2021-11-12 14:24:30 -0300

    creates a reusable function for x trace id header injection

--- src/airlogger/requests.py
+++ src/airlogger/requests.py
@@ -2,67 +2,50 @@
 from airlogger import globals
 import requests
 
+
+def _inject_air_trace(kwargs):
+    headers = kwargs.get('headers')
+    # using "if not" instead .get's default for preventing forced None parameter
+
+    if not isinstance(headers, dict):
+        headers = {}
+
+    headers['X-Air-Trace-Id'] = globals.air_trace_id
+
+    kwargs['headers'] = headers
+    return kwargs
+
+
 class Session(requests.Session):
     def __init__(self):
         super().__init__()
 
     def request(self, method, url, *args, **kwargs):
 
-        if kwargs.get('headers'):
-            kwargs['headers'].update({
-                'X-Air-Trace-Id': globals.air_trace_id,
-            })
-        else:
-            kwargs['headers'] = {'X-Air-Trace-Id': globals.air_trace_id}
-        
+        _inject_air_trace(kwargs)
         return super().request(method, url, *args, **kwargs)
 
+
 def get(url, *args, **kwargs):
-    if kwargs.get('headers'):
-        kwargs['headers'].update({
-            'X-Air-Trace-Id': globals.air_trace_id,
-        })
-    else:
-        kwargs['headers'] = {'X-Air-Trace-Id': globals.air_trace_id}
-    
+    _inject_air_trace(kwargs)
     return requests.get(url, *args, **kwargs)
 
+
 def post(url, *args, **kwargs):
-    if kwargs.get('headers'):
-        kwargs['headers'].update({
-            'X-Air-Trace-Id': globals.air_trace_id,
-        })
-    else:
-        kwargs['headers'] = {'X-Air-Trace-Id': globals.air_trace_id}
-    
+    _inject_air_trace(kwargs)
     return requests.post(url, *args, **kwargs)
 
+
 def put(url, *args, **kwargs):
-    if kwargs.get('headers'):
-        kwargs['headers'].update({
-            'X-Air-Trace-Id': globals.air_trace_id,
-        })
-    else:
-        kwargs['headers'] = {'X-Air-Trace-Id': globals.air_trace_id}
-    
+    _inject_air_trace(kwargs)
     return requests.put(url, *args, **kwargs)
 
+
 def delete(url, *args, **kwargs):
-    if kwargs.get('headers'):
-        kwargs['headers'].update({
-            'X-Air-Trace-Id': globals.air_trace_id,
-        })
-    else:
-        kwargs['headers'] = {'X-Air-Trace-Id': globals.air_trace_id}
+    _inject_air_trace(kwargs)
     return requests.delete(url, *args, **kwargs)
 
+
 def patch(url, *args, **kwargs):
-    if kwargs.get('headers'):
-        kwargs['headers'].update({
-            'X-Air-Trace-Id': globals.air_trace_id,
-        })
-    else:
-        kwargs['headers'] = {'X-Air-Trace-Id': globals.air_trace_id}
+    _inject_air_trace(kwargs)
     return requests.patch(url, *args, **kwargs)
-
-
